01e6e6182a8bc02c2f6c31f142e1054919afa949
fbgencer <fbgencer8@gmail.com>
2021-11-30 19:35:59 +0300

    added list

--- markdownipy.py
+++ markdownipy.py
@@ -15,7 +15,9 @@ class markdownipy:
 			return self.start + m + self.end
 
 	bold = emphasis("__","__")
+	b = bold
 	italic = emphasis("_","_")
+	i = italic
 	code = emphasis('`','`')
 	codeb = emphasis('```','```')
 	h1 = emphasis('#'*1+' ','')
@@ -25,6 +27,11 @@ class markdownipy:
 	h5 = emphasis('#'*5+' ','')
 	h6 = emphasis('#'*6+' ','')
 
+	link = emphasis('<','>')
+	img = emphasis('![](',')')
+
+	available_bullets = ['*','-']
+
 	def __init__(self):
 		print("Hello you have created me")
 		self.doc = ""
@@ -33,9 +40,18 @@ class markdownipy:
 		if(isinstance(x,str)):
 			self.doc += x + "\n\n"
 		elif(isinstance(x,list)):
-			for ln in range(len(x)):
-				line = f"{ln+1}. {x[ln]}\n"
-				self.doc += line + "\n\n"
+			assert(len(x))
+			if(x[0] in self.available_bullets):
+				#This is bullet'in header, don't put number
+				bullet_type = x[0]
+				x = x[1:]
+				for ln in x:
+					line = f"{bullet_type} {ln}\n"
+					self.doc += line + "\n\n"
+			else:
+				for ln in range(len(x)):
+					line = f"{ln+1}. {x[ln]}\n"
+					self.doc += line + "\n\n"
 	
 	def __rshift__(self,file_name:str):
 		if(not file_name.endswith(".md")):
@@ -52,9 +68,7 @@ class markdownipy:
 			self.put(line)
 
 	def lists(self,str_list):
-		for ln in range(len(str_list)):
-			line = f"{ln+1}. {str_list[ln]}\n"
-			self.put(line)
+		self << str_list
 
 	def image(self,img_path):
 		line = f"![foo]({img_path})"
@@ -80,6 +94,16 @@ md << [
 	"fbg3"
 ]
 
+md << [
+	'*',
+	"bullet1",
+	"bullet2",
+	"bullet3"
+]
+
+md << ("https://github.com/fbgencer/markdownipy" | md.link)
+
+md << ("example_logo.png" | md.img)
 
 md.image("example_logo.png")
 md >> "myexample"
--- myexample.md
+++ myexample.md
@@ -17,5 +17,18 @@ _fbg_
 3. fbg3
 
 
+* bullet1
+
+
+* bullet2
+
+
+* bullet3
+
+
+<https://github.com/fbgencer/markdownipy>
+
+![](example_logo.png)
+
 ![foo](example_logo.png)
 
